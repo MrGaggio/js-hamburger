cfbb494c5a212b1c3f5e1b929b1842ef239bc48a
MrGaggio <steruggiu@gmail.com>
2021-10-12 17:27:27 +0100

    selezionati div e icona hamburger

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,26 @@
 //Mostrare / nascondere il menù principale
+
+/*
+
+-selezionare l'icona hamburger
+
+-selezionare div hamburger
+
+
+-aggiungere una classe all'hamburger icona
+
+-quando clicco sull'icona far sparire l'hamburger
+
+-quando clicco sul div haburger far apparire l'hambur
+*/
+
+
+/* Selezionare il div hamburger*/
+
+let hamburgerDiv = document.getElementsByClassName("hamburger-menu");
+console.log(document.getElementsByClassName("hamburger-menu"));
+
+/* -selezionare l'icona hamburger */
+
+let hamburgerIcon = document.getElementsByClassName("fas fa-bars")
+console.log(document.getElementsByClassName("fas fa-bars"));
