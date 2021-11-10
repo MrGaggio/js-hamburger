cc77901de85970ee3ab29400ff8658afe4c28d8c
MrGaggio <steruggiu@gmail.com>
2021-11-10 16:31:23 +0000

    Rivisto esercizio

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,52 @@
-//Mostrare / nascondere il menù principale
-
-/*
-
--selezionare div hamburger
+// Nell'esercizio è richiesto di mostrare il menu a tendina quando si clicca sul menu hamburger e di chiudere il menu cliccando sulla x
 
 
+/* Strumenti:
+
+- querySelector ()
+- addEventListener
 
 
--quando clicco sull'icona far sparire l'hamburger e mostrare il contenuto del menu (menu hamburger display-none)
+ */
 
--quando clicco sulla X chiudere il menu per far riapparire il menu hamburger
-*/
+
+//Selezionare elementi della DOM
+
+const hamburgerIconElement = document.querySelector('.header-right > a') 
+const hamburgerMenuElement = document.querySelector('.hamburger-menu')
+const closeElement = document.querySelector(' .close')
+
+
+
+//Al click di hamburgerElement deve comparire il menu a tendina
+
+hamburgerIconElement.addEventListener('click', function () {
+    /* console.log('cliccato'); funziona */
+    hamburgerMenuElement.style.display = 'block'
+
+})
+
+
+//Al click della X deve scomparire il menu a tendina
+
+closeElement.addEventListener('click',function () {
+    hamburgerMenuElement.style.display = 'none'
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,19 +56,20 @@
 
 /* Selezionare il div hamburger*/
 
-const hamburgerMenuElement = document.querySelector(" .hamburger-menu");
+/* const hamburgerMenuElement = document.querySelector(" .hamburger-menu");
 const hamburgerIcon = document.querySelector(" .fas.fa-bars");
 const closeIcon = document.querySelector(" .close");
-console.log(hamburgerMenuElement, hamburgerIcon);
+console.log(hamburgerMenuElement, hamburgerIcon); */
 
 /* cliccare all'interno del div hamburger */
-hamburgerIcon.addEventListener('click', function (){
-    console.log("clicca"); //errore che non riesco a risolvere ho anche provato a copiare pari pari la stringa di codice da w3shool
+/* hamburgerIcon.addEventListener('click', function (){
+    console.log("clicca"); 
     hamburgerMenuElement.style.display = 'block'
-})
+}) */
     
 //quando il menu è aperto cliccare sulla x per chiuderlo
 
-closeIcon.addEventListener('click', function (){
+/* closeIcon.addEventListener('click', function (){
     hamburgerMenuElement.style.display = 'none'
 })
+ */
